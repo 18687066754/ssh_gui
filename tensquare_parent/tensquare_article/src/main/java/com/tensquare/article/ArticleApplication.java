fabb6d810b05672f8eb0a1892c0a6b9a5beb0485
git <121>
2019-05-22 23:59:18 +0800

    zzzzzz

--- tensquare_parent/tensquare_article/src/main/java/com/tensquare/article/ArticleApplication.java
+++ tensquare_parent/tensquare_article/src/main/java/com/tensquare/article/ArticleApplication.java
@@ -9,6 +9,7 @@ public class ArticleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ArticleApplication.class, args);
+		System.out.printIn("HelloWorld11111111");
 	}
 
 	@Bean
